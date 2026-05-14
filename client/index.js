4c05cd35e39c5171c7c2684b07f065787ff6493c
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-05-14 16:54:44 +0100

    One .gitignore in root, package.json & package-lock.json in root as devcontainer expects this location. Add an express server to allow serve frontned and client without cors. Refactor to ES6 syntax (curren best practice). Runs as expected

--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
+import { io } from "https://cdn.socket.io/4.7.2/socket.io.esm.min.js";
+
 const socket = io();
-socket.on("connect", (response) => {
-  console.log("response");
+
+socket.on("connect", () => {
+  console.log("Connected using ES6 Modules:", socket.id);
 });
 
 socket.on("disconnect", (reason, details) => {
--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
-// https://socket.io/docs/v4/server-initialization/
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+// Serve static frontend files
+app.use(express.static(join(__dirname, "../client")));
 
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 });
 
+// Port 3000 forwarded in devcontainer.json
 httpServer.listen(3000, "0.0.0.0", () => {
-  console.log("Server is connected");
+  console.log("Server running at http://localhost:3000");
 });
