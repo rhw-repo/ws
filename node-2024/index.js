beec43bee9bbd29d549720291b0628c9ff840894
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-06-08 19:03:33 +0100

    Add hteml pages in public and serve up in index.js using fs

--- node-2024/index.js
+++ node-2024/index.js
@@ -1,12 +1,44 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
   res.setHeader("Content-Type", "text/html");
   // unneccessary to end in Express
   //res.statusCode = 404;
-  res.writeHead(200, "Content-Type", "application/json");
-  res.end(JSON.stringify({ message: "change" }));
+
+  //console.log(req.url);
+  //console.log(req.method);
+
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("not found");
+      }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, "Content-Type", "application/json");
+    res.end(JSON.stringify({ message: "not found" }));
+  }
 });
 
 server.listen(PORT, () => {
