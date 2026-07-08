9337b6a7471f63cd41cfa255d08786432c507efe
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-07-08 19:06:24 +0100

    Add html-loader, an image file in webp format with styling in styles.css. Add config for webpack to be able to load images from the index.js file. Add webpack-dev-server to enable equivalent of hot reloading instead of having to rebundle very time a change is made.

--- javascript/odin-webpack/src/index.js
+++ javascript/odin-webpack/src/index.js
@@ -1,3 +1,12 @@
+import flouffyImage from "./flouffy-image.webp";
+
+import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+
+const image = document.createElement("img");
+image.src = flouffyImage;
+image.classList.add("flouffy-pic");
+
+document.body.appendChild(image);
