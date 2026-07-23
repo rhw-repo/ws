c03dac8989c2ef875e33c4b69f9c4c85ecf8f21c
Ruth Westnidge Brown <85512549+rhw-repo@users.noreply.github.com>
2026-07-23 14:13:20 +0000

    Initial commit frontend directory. Vite React boilerpate cleared, css remains for potential future use

---
+++ react_challenges/react-props/frontend/ruth-westnidge-brown/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+        
+        </div>
+     
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
