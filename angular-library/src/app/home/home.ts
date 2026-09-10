5c55aae39207a5be25afddbc13ae870c4637827f
Ruth Westnidge <rwestnidge@gmail.com>
2026-09-10 17:19:49 +0000

    Add home directory with first component from running pnpm exec with ng g c

---
+++ angular-library/src/app/home/home.ts
@@ -0,0 +1,11 @@
+// https://angular.dev/style-guide#introduction
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
