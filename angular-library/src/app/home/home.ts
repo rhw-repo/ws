6d0281ab15a771f460fe84bafb79d1cd0e251211
Ruth Westnidge <rwestnidge@gmail.com>
2026-09-11 13:05:15 +0000

    Add demo of binding proerties and methods between home.ys and home.html. Not best practice just demo. IRL use Signals

--- angular-library/src/app/home/home.ts
+++ angular-library/src/app/home/home.ts
@@ -9,4 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  protected title = 'Chingu Devs Book Club';
+  protected subtitle = 'Reading List';
+  protected subtitleParagraph = 'Your personal shelf, shared with the club. Move a book as you go.';
+  // Demo of property binding - set disabled property in an array to isDsiabled in HTML and binds it to false
+  protected isDisabled = false;
+
+  protected onClick(): void {
+    console.log('Button clicked');
+    this.isDisabled = !this.isDisabled;
+  }
+}
